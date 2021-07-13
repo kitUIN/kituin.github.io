4823a95ab6384a11f6bb4ce5344f4f48177445a4
KitUIN <kulujun@gmail.com>
2021-07-14 00:25:06 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github
-echo 'www.kituin.cf' > CNAME
+echo 'kulujun.cf' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:kitUIN/kitUIN.github.io.git
--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,7 +1,7 @@
 // head
 module.exports = [
   // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-  ['link', { rel: 'icon', href: '/img/bitbug_favicon.ico' }], //favicons，资源放在public文件夹
+  ['link', { rel: 'icon', href: '/bitbug.ico' }], //favicons，资源放在public文件夹
   [
     'meta',
     {
