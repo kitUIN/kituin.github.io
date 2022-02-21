7a66a83cebb12ed346c5dd54f03840ab9cc108a0
kitUIN <kulujun@gmail.com>
2022-02-21 17:53:12 +0800

    update

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -7,36 +7,20 @@ module.exports = [
   // }],
 
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+  ['reading-progress'],
+  ['fulltext-search'],
   [
-    'thirdparty-search',
-    {
-      thirdparty: [
-        // 可选，默认 []
-        {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
-        },
-        {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q=',
-        },
-        {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-        },
-      ],
-    },
-  ],
-
+      'vuepress-plugin-comment-plus',
+      {
+	     choosen: 'waline',
+         // options选项中的所有参数，会传给Waline的配置
+         options: {
+           el: '#valine-vuepress-comment',
+           serverURL: 'https://kituin-comments.vercel.app/', //  例如 "https://***.vercel.app/"
+	       path: '<%- frontmatter.commentid || frontmatter.permalink %>'
+         }
+      }
+    ],
   [
     'one-click-copy',
     {
@@ -112,26 +96,6 @@ module.exports = [
     'vuepress-plugin-baidu-tongji', // 百度统计
     {
       hm: '503f098e7e5b3a5b5d8c5fc2938af002',
-    },
-  ],
-  [
-    'vuepress-plugin-comment', // 评论
-    {
-      choosen: 'gitalk',
-      options: {
-        clientID: '55983b50ec04628f6c49',
-        clientSecret: 'f7117f3bbfeed95ae798d70faf4c53ff686547c3',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'kitUIN', // GitHub仓库所有者
-        admin: ['kitUIN'], // 对仓库有写权限的人
-        // distractionFreeMode: true,
-        pagerDirection: 'last', // 'first'正序 | 'last'倒序
-        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-        body:
-          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-      },
     },
   ],
   [
